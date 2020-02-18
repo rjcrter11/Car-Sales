616f469d662dadeb5aff73cac112c4f6013c8c6a
rjcrter11 <rjcrter11@gmail.com>
2020-02-18 15:33:37 -0600

    got everything connected and set up...maybe

---
+++ src/reducers/addPriceReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  additionalPrice: 0
+};
+
+export const addPriceReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
